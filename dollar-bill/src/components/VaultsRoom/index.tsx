f7a2965f1a183fa7a2739a7430ea55f6b4a9aaf9
RolandoDrRobot <rolando.arguello.email@gmail.com>
2022-06-13 23:58:53 -0600

    We set loading state

--- dollar-bill/src/components/VaultsRoom/index.tsx
+++ dollar-bill/src/components/VaultsRoom/index.tsx
@@ -10,40 +10,70 @@ import './main.css';
 function VaultsRoom() {
 
   const { account } = useWeb3React();
+  let [isLoading, setIsLoading] = React.useState<boolean>(true);
 
-  const { 
-    vaults
-    } = React.useContext(globalContext);
+  const { vaults } = React.useContext(globalContext);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    if (vaults.length && account) {
+      setIsLoading(false);
+
+      Object.entries(vaults[1].balance).map(item => {
+        console.log(item)
+      })
+      
+      Object.entries(vaults[1].balance).forEach(item => {
+        console.log(item)
+      })
+      
+      for (const item of Object.entries(vaults[1].balance)) {
+        console.log(item)
+      }
+    }
+  }, [vaults, account]);
 
   return (
     <>
       <div className="vault mt-3">
         { 
-          vaults ? vaults.map(function(item:any, i:any) {
-            return (
-              <Link to={`/trading/${i}`} key={i} className="main-button d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center">
-                  <img 
-                    src={
-                      item.exchange === 'binance' ? binanceIcon
-                      : item.exchange === 'ftx' ? ftxIcon
-                      : ''
-                    }
-                    className="exchange-logo" 
-                    alt="" 
-                  />
-                  {item.name}
-                </div>
-                <div>
-                  $ {item.usdTotalBalance.toFixed(2)}
-                </div>
-              </Link>
-            )
-          }) : <Loading />
+          isLoading 
+            ? <>
+                <Loading />
+                <p className="info-text">
+                  { account ? 'We are loading your vaults' : 'Connect your wallet' }
+                </p>
+              </>
+            : <>
+                {
+                  vaults.map(function(item:any, i:any) {
+                    return (
+                      <Link to={`/trading/${i}`} key={i} className="main-button d-flex align-items-center justify-content-between">
+                        <div className="d-flex align-items-center">
+                          <img 
+                            src={
+                              item.exchange === 'binance' ? binanceIcon
+                              : item.exchange === 'ftx' ? ftxIcon
+                              : ''
+                            }
+                            className="exchange-logo" 
+                            alt="" 
+                          />
+                          {item.name}
+                        </div>
+                        <div>
+                          $ {item.usdTotalBalance.toFixed(2)}
+                        </div>
+                      </Link>
+                    )
+                  })
+                }
+                <p className="info-text">
+                  { account ? 'Select a vault to manage' : 'Connect your wallet' }
+                </p>
+              </>
         }
-        <p className="info-text">
-          { account ? 'Select a vault to manage' : 'Connect wallet' }
-        </p>
+        
       </div>
     </>
   )
